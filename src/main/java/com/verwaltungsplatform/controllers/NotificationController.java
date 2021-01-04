6f3d8debf1d0e4b41cec6523d3f375ce173952d4
Entwicklung <Entwicklung@Ryzen2070.fritz.box>
2021-01-04 13:08:18 +0100

    Erstellen des NotificationControllers

--- src/main/java/com/verwaltungsplatform/controllers/NotificationController.java
+++ src/main/java/com/verwaltungsplatform/controllers/NotificationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,41 +24,99 @@ public class NotificationController {
 	
 	@Autowired
 	private NotificationRepository notificationRepo;
+
 	
-	
-	
-	// Jedem Nutzer, der im System angemeldet ist, werden auf der Startseite die aktuellen Ankündigungen angezeigt
-	// Das  „Sekretariat“  kann Ankündigungen   verwalten   und   verfassen (Sichtbarkeit  nach  Rollen)
-	// Nach Erstellung einer Ankündigung ist es möglich die Ankündigung zu löschen
-	// „Lehrende“  können klassenspezifische   Ankündigungen   verfassen
-	
-	
-	
-	
-	
-	
-	// Anzeigen vpn Ankündigungen funktioniert nicht
+	// Anzeigen aller betreffenden Ankündigungen als ein Schüler
 	@GetMapping("/lernender/ankuendigungen")
 	@ResponseBody
-	public List<Notification> getNotificationsStudent() {
+	public List<Notification> getNotificationsStudent(int studentId) {
 		
-		List<Notification> notification = notificationRepo.findAll();
-		
-		//Methode in nächster Zeile funktioniert jetzt, filtert welche Ankündigungen angezeigt werden, braucht @param studentID
-		//List<Notification> notification = notificationService.getAllNotificationsRoleAndClass(studentId);
+		List<Notification> notification = notificationService.getAllNotificationsRoleAndClass(studentId);
 		
 		return notification;
 	}
 	
+	// Anzeigen aller betreffenden Ankündigungen als ein Lehrer
+	// Funktioniert nicht für mehrere Klassen
+	@GetMapping("/lehrender/ankuendigungen")
+	@ResponseBody
+	public List<Notification> getNotificationsTeacher(int teacherId) {
+		
+		List<Notification> notification = notificationService.getAllNotificationsRoleAndClass(teacherId);
+		
+		return notification;
+	}
 	
-	// Erstellen einer neuen Ankündigung funktioniert
+	// Anzeigen aller betreffenden Ankündigungen als ein Elternteil
+	@GetMapping("/eltern/ankuendigungen")
+	@ResponseBody
+	public List<Notification> getNotificationsParent(int parentId) {
+		
+		List<Notification> notification = notificationService.getAllNotificationsRoleAndClass(parentId);
+		
+		return notification;
+	}
+	
+	// Anzeigen aller betreffenden Ankündigungen als ein Sekretariatsmitglied
+	@GetMapping("/sekretariat/ankuendigungen")
+	@ResponseBody
+	public List<Notification> getNotificationsSecretariat(int secretariatId) {
+		
+		List<Notification> notification = notificationService.getAllNotRole(secretariatId);
+		notification.addAll(notificationService.getAllNotifications());
+		
+		return notification;
+	}
+	
+	// Anzeigen aller Ankündigungen als Sekretariatsmitglied
+	@GetMapping("/sekretariat/alleankuendigungen")
+	@ResponseBody
+	public List<Notification> getAllNotifications() {
+		
+		List<Notification> notification = notificationRepo.findAll();
+		
+		return notification;
+	}
+	
+	// Erstellen einer neuen allgemeinen Ankündigung als Sekretariatsmitglied
 	@PostMapping("/sekretariat/neueankuendigungallgemein")
 	@ResponseBody
-	public Notification addNotification(int user, Date start, Date end, String content) {
+	public Notification addNotificationGeneral(int user, Date start, Date end, String content) {
 		
 		Notification newNotification = new Notification(user, start, end, content);
 		notificationRepo.save(newNotification);
 		return newNotification;
+	}
+	
+	// Erstellen einer neuen rollenspezifischen Ankündigung als Sekretariatsmitglied
+	@PostMapping("/sekretariat/neueankuendigungrolle")
+	@ResponseBody
+	public Notification addNotificationRole(int user, Date start, Date end, String role, String content) {
+		
+		Notification newNotification = new Notification(user, start, end, role, content);
+		notificationRepo.save(newNotification);
+		return newNotification;
+	}
+	
+	// Erstellen einer neuen klassenspezifischen Ankündigung als Lehrer
+	@PostMapping("/lehrender/neueankuendigungklasse")
+	@ResponseBody
+	public Notification addNotificationClass(int user, String classId, String content, Date start, Date end) {
+		
+		Notification newNotification = new Notification(user, classId, content, start, end);
+		notificationRepo.save(newNotification);
+		return newNotification;
+	}
+	
+	// Löschen einer Ankündigung als Sekretariatsmitglied
+	@DeleteMapping("/sekretariat/ankuendigungloeschen/{notificationId}")
+	@ResponseBody
+	public String deleteNotification(@PathVariable("notificationId") int notificationId) {
+		
+		Notification notification = notificationRepo.getOne(notificationId);
+		notificationRepo.delete(notification);
+		String response = new String("Die Ankündigung wurde gelöscht");
+		return response;
 	}
 
 }
